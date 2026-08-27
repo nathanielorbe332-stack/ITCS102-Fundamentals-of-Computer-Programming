a4b860cef603b39f7626a277562f39f41a6ef251
nathanielorbe332-stack <nathanielorbe332@gmail.com>
2026-08-27 20:21:06 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,11 @@
+#escape sequence -> to modify the behavior of line formatted string
+# \n - new line
+# \t - tab space 
+# \r - carriage return / deprecated (obsolete)
+# \b - back space 
+# \\ - insert slash
+# \" - insert double qoutation
+
+phrase1="no to \r pax silica \n\t\t\t here in\b our country\\Philippines \""
+
+print(phrase1)
