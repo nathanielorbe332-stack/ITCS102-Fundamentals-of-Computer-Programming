54153fef9df2cd41d46b0a540d99a0bba680506c
nathanielorbe332-stack <nathanielorbe332@gmail.com>
2026-08-27 17:43:38 +0800

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,23 @@
+Nathaniel Orbe
+Nathaniel Orbe
+Nathaniel Orbe
+Nathaniel Orbe
+Nathaniel Orbe
+Nathaniel Orbe
+Nathaniel Orbe
+Nathaniel Orbe
+Nathaniel Orbe
+Nathaniel Orbe
+
+
+Bachelor of Science in Information Technology - 1B
+Bachelor of Science in Information Technology - 1B
+Bachelor of Science in Information Technology - 1B
+Bachelor of Science in Information Technology - 1B
+Bachelor of Science in Information Technology - 1B
+Bachelor of Science in Information Technology - 1B
+Bachelor of Science in Information Technology - 1B
+Bachelor of Science in Information Technology - 1B
+Bachelor of Science in Information Technology - 1B
+Bachelor of Science in Information Technology - 1B
+Bachelor of Science in Information Technology - 1B
