22253786abf1465474d01711bfd4efce863960ae
nathanielorbe332-stack <nathanielorbe332@gmail.com>
2026-09-14 17:02:28 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,37 @@
+Favorites= "" #unknown string
+
+
+f = input("What are your Favorite Games? --> ")
+Favorites += f + ", "
+
+f = input("What else? --> ")
+Favorites += f + ", "
+
+f = input("What else? --> ")
+Favorites += f + ", "
+
+f = input("What else? --> ")
+Favorites += f + ", "
+
+f = input("What else? --> ")
+Favorites += f + " and "
+
+f = input("What else? --> ")
+Favorites += f + ". "
+
+f = input("What else? --> ")
+Favorites += f + ". "
+
+f = input("What else? --> ")
+Favorites += f + ". "
+
+f = input("What else? --> ")
+Favorites += f + ". "
+
+f = input("What else? --> ")
+Favorites += f + ". "
+
+f = input("What else? --> ")
+Favorites += f + ". "
+
+print("My favorite games are {", Favorites ,"}")
