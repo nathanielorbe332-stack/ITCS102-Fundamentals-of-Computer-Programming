a9bb62a02dd8ef869aa7d071c1807b0e8d305727
nathanielorbe332-stack <nathanielorbe332@gmail.com>
2026-09-14 16:44:31 +0800

    Add files via upload

---
+++ Code_challenge1.py
@@ -0,0 +1,14 @@
+print(
+"\t\t\t\t\t\t\t\b\b\b\b*\n"
+"\t\t\t\t\t*\t\t\t*\n"
+"\t\t\t\t*\t\t\t\t\t*\n"
+"\t\t\t*\t\t\t\t\t\t\t*\n"
+"\t\t*\n"
+"*\t*\t*\t\t\t\t\t\t\t\t\t*\t*\t*\n"
+"*\t*\t*\t\t\t\t   MY NAME IS\t\t\t\t*\t*\t*\n"
+"*\t*\t*\t\t\t\t   Nathaniel B. Orbe\t\t\t*\t*\t*\n"
+"*\t*\t*\t\t\t\t    BSIT-1B\t\t\t\t*\t*\t*\n"
+"*\t*\t*\t\t\t\t\t\t\t\t\t*\t*\t*\n"
+"*\t*\t*\t\t\t\t\t\t\t\t\t*\t*\t*\n"
+"*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\n"
+"*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t*\n")
