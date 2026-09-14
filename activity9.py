0921499baed5b46009634af338454027a7a6a5ee
nathanielorbe332-stack <nathanielorbe332@gmail.com>
2026-09-14 17:05:51 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,24 @@
+# operator activity
+
+x = 6
+y = 7
+
+passwordI = "on"
+passwordII = "in"
+
+#1
+print(x > y)
+
+#2
+print(x < y)
+
+#3
+print(x >= y)
+
+#4
+print(x <= y)
+
+#5
+print( x== y)
+
+print(passwordI == passwordII)
