4f7efb7a1fb52cd62f18a5315c9d143e244c914c
nathanielorbe332-stack <nathanielorbe332@gmail.com>
2026-09-14 16:42:55 +0800

    Add files via upload

---
+++ Activity5.py
@@ -0,0 +1,15 @@
+#operators activity
+
+a = 6
+b = 1
+sum = a + b
+diff = a - b
+multiplied = a*b
+division = a//b
+modulo = a%b
+
+print("The sum of ",a," and", b," is",sum)
+print("The difference of ",a," and", b,"is",diff)
+print("The multiplied of ",a," and", b,"is",multiplied)
+print("The division of ",a," and", b,"is",division)
+print("The modulo of ",a," and", b,"is",modulo)
