43924771a3fc3f0d882ac4a603bee67005081bd7
nathanielorbe332-stack <nathanielorbe332@gmail.com>
2026-09-14 16:42:12 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,28 @@
+total = 28
+
+J = eval(input("\nEnter any number:"))
+total = total + J
+
+N = eval(input("\nany number:"))
+total += N
+
+D = eval(input("\nany number:"))
+total -= D
+
+R = eval(input("\nany number:"))
+total -= R
+
+I = eval(input("\nany number:"))
+total -= I
+
+C = eval(input("\nany number:"))
+total *= C
+
+K = eval(input("\nany number:"))
+total += K
+
+D = eval(input("\nany number:"))
+total *= D
+
+print(type(total))
+print("The total is", total)
