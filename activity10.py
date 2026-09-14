00e3e7731fe2ed1e876cdf03f9d4d9a728e4810e
nathanielorbe332-stack <nathanielorbe332@gmail.com>
2026-09-14 17:10:55 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,12 @@
+# basic if else program
+
+username = "Orbepogi"
+password = "pogisiOrbe"
+
+u = input("input USERNAME ===>")
+p = input("input PASSWORD ===>")
+
+if u == username and p == password :
+	print("username and password correct")
+else:
+	print("DENIED")
